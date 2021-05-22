d85d07b62e60443ec3042f044c2c3e51ec242e98
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-21 17:50:52 -0700

    First commit. Initial layout and preview layout for Bullseye game.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50.0), in: 1.0...100.0)
+                Text("100")
+            }
+            Button(action: {}) {
+                Text("Hit Me!")
+            }
+
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
