18308aa10a2b4249322b3bd67722d1ebb803b738
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-23 06:46:25 -0700

    Change difference to use abs() function

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -13,10 +13,7 @@ struct Game {
     var round: Int = 1
     
     func points(sliderValue: Int) -> Int {
-        var difference: Int = self.target - sliderValue
-        if difference < 0 {
-            difference *= -1
-        }
+        let difference: Int = abs(self.target - sliderValue)
         let awardedPoints: Int = 100 - difference
         return awardedPoints
     }
