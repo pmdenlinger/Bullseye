f7636ab395762366c5173ffcdaaa025c502245dc
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-23 07:39:27 -0700

    Embed VStack in ZStack and add background color for full screen

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -15,46 +15,51 @@ struct ContentView: View {
     @State private var game: Game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .font(.largeTitle)
-                .bold()
-                .fontWeight(.black)
-                .kerning(-1.0)
-            HStack {
-                Text("1")
+        ZStack {
+            // rgb(243, 248, 253)
+            Color(red: 243.0 / 255.0, green: 248.0 / 255.0, blue: 253.0 / 255.0)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target))
+                    .font(.largeTitle)
                     .bold()
                     .fontWeight(.black)
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
-                    .fontWeight(.black)
-            }
-            .padding()
-            Button(action: {
-                self.alertIsVisible = true
-            }) {
-                Text("HIT ME")
-                    .bold()
-                    .font(.title3)
-            }
-                .padding(20.0)
-                .background(Color.blue)
-                .foregroundColor(Color.white)
-                .cornerRadius(21.0)
-            
-                .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue: Int = Int(sliderValue.rounded())
-                return Alert(title: Text("Hello there!"), message:Text("Theslider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton:.default(Text("Awesome!")))
-            })
+                    .kerning(-1.0)
+                HStack {
+                    Text("1")
+                        .bold()
+                        .fontWeight(.black)
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    Text("100")
+                        .bold()
+                        .fontWeight(.black)
+                }
+                .padding()
+                Button(action: {
+                    self.alertIsVisible = true
+                }) {
+                    Text("HIT ME")
+                        .bold()
+                        .font(.title3)
+                }
+                    .padding(20.0)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(21.0)
+                
+                    .alert(isPresented: $alertIsVisible, content: {
+                    let roundedValue: Int = Int(sliderValue.rounded())
+                    return Alert(title: Text("Hello there!"), message:Text("Theslider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton:.default(Text("Awesome!")))
+                })
 
 
+            }
         }
         
     }
