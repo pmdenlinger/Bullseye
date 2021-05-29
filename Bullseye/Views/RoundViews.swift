f5f73da3b6a92890299f6fc130fd6b3c6b5e3e0f
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-29 09:43:59 -0700

    Add modifiers to images; still cannot change ButtonStrokeColor to #DOE4FA in Assets. Change later when possible.

--- Bullseye/Views/RoundViews.swift
+++ Bullseye/Views/RoundViews.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct RoundImageViewStroked: View {
+struct RoundedImageViewStroked: View {
     var systemName: String
     
     var body: some View {
@@ -15,6 +15,10 @@ struct RoundImageViewStroked: View {
             .font(.title)
             .foregroundColor(Color("TextColor"))
             .frame(width: 56.0, height: 56.0)
+            .overlay(
+              Circle()
+                .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0)
+        )
     }
 }
 
@@ -24,23 +28,28 @@ struct RoundedImageViewFilled: View {
     var body: some View {
         Image(systemName: systemName)
             .font(.title)
-            .foregroundColor(Color("TextColor"))
+            .foregroundColor(Color("ButtonFilledTextColor"))
             .frame(width: 56.0, height: 56.0)
+            .background(Circle().fill(Color("ButtonFilledBackgroundColor"))
+            )
     }
 }
 
 struct PreviewView: View {
-    var body: some View {
-        VStack(spacing: 10) {
-            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
-            RoundedImageViewStroked(systemName: "list.dash")
-        }
+  var body: some View {
+    VStack(spacing: 10) {
+      RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+      RoundedImageViewFilled(systemName: "list.dash")
     }
+  }
 }
 
 struct RoundViews_Previews: PreviewProvider {
     static var previews: some View {
-        PreviewView()
+        Group {
+            PreviewView()
+            PreviewView()
+        }
         PreviewView()
             .preferredColorScheme(.dark)
     }
