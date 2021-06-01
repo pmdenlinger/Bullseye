b823a95341b73ab45a175c74c65904c427b2b5fd
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-31 17:25:48 -0700

    Add tests for testLeaderboard(). Add LeaderboardEntry method to Game.swift.

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+struct LeaderboardEntry {
+    let score: Int
+    let date: Date
+}
+
 struct Game {
     var target = Int.random(in: 1...100)
     var score = 0
     var round = 1
+    var leaderboardEntries: [LeaderboardEntry] = []
     
     func points(sliderValue: Int) -> Int {
         let difference = abs(target - sliderValue)
@@ -25,9 +31,15 @@ struct Game {
         return 100 - difference + bonus
     }
     
+    mutating func addToLeaderboard(point: Int) {
+        leaderboardEntries.append(LeaderboardEntry(score: point, date: Date()))
+        leaderboardEntries.sort { $0.score > $1.score }
+    }
+    
     mutating func startNewRound(points: Int) {
         score += points
         round += 1
+        addToLeaderboard(point: points)
     }
     
     mutating func restart() {
